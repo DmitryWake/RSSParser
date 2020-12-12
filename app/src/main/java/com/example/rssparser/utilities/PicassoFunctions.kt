7583f136890a14fcf5354e41584f5d3051a314e5
Dmitry_Wake <nikolaevskiy2001@gmail.com>
2020-12-12 17:42:38 +0300

    Небольшой рефакторинг

--- app/src/main/java/com/example/rssparser/utilities/PicassoFunctions.kt
+++ app/src/main/java/com/example/rssparser/utilities/PicassoFunctions.kt
@@ -1,13 +1,11 @@
 package com.example.rssparser.utilities
 
 import android.widget.ImageView
-import androidx.databinding.BindingAdapter
 import com.example.rssparser.R
 import com.squareup.picasso.Picasso
 
-// Загружаем фото по url с помощью DataBinding
-@BindingAdapter("app:url")
 fun loadImage(view: ImageView, url: String?) {
-    if (url != null && url.isNotEmpty())
+    if (url != null && url.isNotEmpty()) {
         Picasso.get().load(url).fit().placeholder(R.drawable.ic_base_image).into(view)
+    }
 }
