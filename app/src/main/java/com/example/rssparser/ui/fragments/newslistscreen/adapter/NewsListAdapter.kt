0194ca26524ba33caea316c5b27947bb8f89064e
Dmitry_Wake <nikolaevskiy2001@gmail.com>
2020-12-12 16:53:28 +0300

    Адаптер без ViewModel

--- app/src/main/java/com/example/rssparser/ui/fragments/newslistscreen/adapter/NewsListAdapter.kt
+++ app/src/main/java/com/example/rssparser/ui/fragments/newslistscreen/adapter/NewsListAdapter.kt
@@ -1,12 +1,13 @@
 package com.example.rssparser.ui.fragments.newslistscreen.adapter
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.example.rssparser.R
-import com.example.rssparser.databinding.NewsItemBinding
 import com.example.rssparser.models.NewsModel
+import com.example.rssparser.utilities.loadImage
+import kotlinx.android.synthetic.main.news_item.view.*
 
 class NewsListAdapter : RecyclerView.Adapter<NewsListAdapter.NewsListHolder>() {
 
@@ -14,14 +15,8 @@ class NewsListAdapter : RecyclerView.Adapter<NewsListAdapter.NewsListHolder>() {
     lateinit var onItemClick: (String) -> Unit
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsListHolder {
-        val binding = DataBindingUtil.inflate<NewsItemBinding>(
-            LayoutInflater.from(parent.context),
-            R.layout.news_item,
-            parent,
-            false
-        )
-        // Передаём биндинг для ViewModel
-        return NewsListHolder(binding)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.news_item, parent, false)
+        return NewsListHolder(view)
     }
 
     override fun onBindViewHolder(holder: NewsListHolder, position: Int) {
@@ -35,20 +30,20 @@ class NewsListAdapter : RecyclerView.Adapter<NewsListAdapter.NewsListHolder>() {
         notifyDataSetChanged()
     }
 
-    inner class NewsListHolder(private val binding: NewsItemBinding) :
-        RecyclerView.ViewHolder(binding.root) {
-
-        val viewModel: NewsItemViewModel = NewsItemViewModel()
+    inner class NewsListHolder(private val view: View) :
+        RecyclerView.ViewHolder(view) {
 
         init {
-            binding.newsItemHolderClContainer.setOnClickListener {
+            view.news_item_holder_cl_container.setOnClickListener {
                 onItemClick(dataList[adapterPosition].link)
             }
+
         }
 
         fun bind(newsModel: NewsModel) {
-            viewModel.initViewModel(newsModel)
-            binding.viewModel = viewModel
+            view.news_item_holder_tv_title.text = newsModel.title
+            view.news_item_holder_tv_description.text = newsModel.description
+            loadImage(view.news_item_holder_iv_image, newsModel.imageUrl)
         }
     }
 }
